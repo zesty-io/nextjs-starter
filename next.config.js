e5e23918d6e6988c9b286352fb88c40221f5b030
ardeay <apuzzo@gmail.com>
2022-02-23 22:06:18 -0800

    iclearing the config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "env": {
     "zesty": {
-      "instance_zuid": "test",
-      "stage": "asdasd",
-      "production": "https://www.zesty.io",
+      "instance_zuid": "",
+      "stage": "",
+      "production": "",
       "stage_password": "",
       "auth": "",
       "src_dir": ""
