c629929234733f2cb649bc0dd4b6c6947731f318
Randy Apuzzo <apuzzo@gmail.com>
2022-06-09 11:28:58 -0400

    Main layout

--- layout/Main/Main.js
+++ layout/Main/Main.js
@@ -3,6 +3,7 @@ import { Box, Container } from '@mui/material'
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import { ThemeProvider, createTheme } from '@mui/system';
+
 import Head from 'next/head'
 const theme = createTheme({
   palette: {
@@ -49,10 +50,13 @@ const Main = ({
             <Container >
                 
                 {children}
-                <Box>
-                    <Footer/>
-                </Box>
+                
             </Container>
+            <Box>
+              <Container>
+                <Footer/>
+              </Container>
+            </Box>
         </ThemeProvider>
     )
 }
