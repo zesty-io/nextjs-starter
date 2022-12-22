d52dc373b2a17351d4455cf777dcfdc2cef3f72e
Randy Apuzzo <apuzzo@gmail.com>
2022-12-21 16:25:53 -0800

    Update ZestyTutorial.js

--- components/ZestyTutorial.js
+++ components/ZestyTutorial.js
@@ -192,7 +192,7 @@ export default function ZestyTutorial(props) {
           </Card>
           <br />
           <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea onClick={() => handleClick('https://www.youtube.com/zesty-io')}>
+            <CardActionArea onClick={() => handleClick('https://chat.zesty.io')}>
               <CardMedia
                 component="img"
                 height="140"
